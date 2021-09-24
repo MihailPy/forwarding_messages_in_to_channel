89ac5424db0d33b270d33a50ab9dc0f1863e4bda
MihailPy <mihailpy02@gmail.com>
2021-09-24 15:16:29 +0300

    add two account

--- login.py
+++ login.py
@@ -1,9 +1,19 @@
 import os
-
-from telethon.sync import TelegramClient
 from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
 
-api_id = int(os.getenv("api_id"))
-api_hash = str(os.getenv("api_hash"))
-with TelegramClient(StringSession(), api_id, api_hash) as client:
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_id1 = int(os.getenv("api_id1"))
+api_hash1 = str(os.getenv("api_hash1"))
+with TelegramClient(StringSession(), api_id1, api_hash1) as client:
+    print("Скопируйте код 1 сессии")
+    print(client.session.save())
+
+api_id2 = int(os.getenv("api_id2"))
+api_hash2 = str(os.getenv("api_hash2"))
+with TelegramClient(StringSession(), api_id2, api_hash2) as client:
+    print("Скопируйте код 2 сессии")
     print(client.session.save())
--- main.py
+++ main.py
@@ -5,24 +5,37 @@ from telethon.sessions import StringSession
 
 load_dotenv()
 
-api_id = int(os.getenv("api_id"))
-api_hash = str(os.getenv("api_hash"))
-string_session = str(os.getenv("string_session"))
+api_id1 = int(os.getenv("api_id1"))
+api_hash1 = str(os.getenv("api_hash1"))
+string_session1 = str(os.getenv("string_session1"))
+client1 = TelegramClient(StringSession(string_session1), api_id1, api_hash1)
+my_channel1 = int(os.getenv("my_channel1"))
+sources_of_messages1 = list(map(int, os.getenv("sources_of_messages1").split(",")))
 
-client = TelegramClient(StringSession(string_session), api_id, api_hash)
+api_id2 = int(os.getenv("api_id2"))
+api_hash2 = str(os.getenv("api_hash2"))
+string_session2 = str(os.getenv("string_session2"))
+client2 = TelegramClient(StringSession(string_session2), api_id2, api_hash2)
+my_channel2 = int(os.getenv("my_channel2"))
+sources_of_messages2 = list(map(int, os.getenv("sources_of_messages2").split(",")))
 
-my_channel = int(os.getenv("my_channel"))
-sources_of_messages = list(map(int, os.getenv("sources_of_messages").split(",")))
 
-
-@client.on(events.NewMessage(chats=sources_of_messages))
-async def handler(event):
+@client1.on(events.NewMessage(chats=sources_of_messages1))
+async def handler1(event):
     print("New message")
-    await event.message.forward_to(my_channel)
+    await event.message.forward_to(my_channel1)
 
 
-client.start()
+@client2.on(events.NewMessage(chats=sources_of_messages2))
+async def handler2(event):
+    print("New message")
+    await event.message.forward_to(my_channel2)
+
+
+client1.start()
+client2.start()
 
 if __name__ == '__main__':
     print("Starting")
-    client.run_until_disconnected()
+    client1.run_until_disconnected()
+    client2.run_until_disconnected()
